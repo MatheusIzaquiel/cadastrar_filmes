2be6a735c0ec8b9ddf117a3a84b7a8984eb78f96
MatheusIzaquiel <matheusizaquiel1898@gmail.com>
2025-11-07 18:28:57 -0300

    Adicionando url do back-end

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:"http://localhost:5000",
+  baseURL:"https://api-filmes-j74y.onrender.com/films",
   headers: { "Content-Type": "application/json" }, 
 })
